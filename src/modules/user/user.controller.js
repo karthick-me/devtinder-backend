53eeb066f1df79d55acdc02d4c29de01032f5d07
Karthick <karthickdeveloper72@gmail.com>
2025-07-13 12:32:21 +0530

    refactor(core): apply asyncHandler to user controllers

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -2,45 +2,42 @@ const {
     formatSuccessResponse,
     formatErrorResponse,
 } = require("../../shared/utils");
+const asyncHandler = require("../../shared/utils/asyncHandler");
 const { getUserById, updateUserProfileById } = require("./user.service");
 
-const getLoggedInUserController = async function (request, response) {
-    try {
-        const userId = request.userId;
-        const user = await getUserById(userId);
+const getLoggedInUserController = asyncHandler(async function (
+    request,
+    response
+) {
+    const userId = request.userId;
+    const user = await getUserById(userId);
 
-        const successResponse = formatSuccessResponse({
-            message: "User profile fetched successfully",
-            data: {
-                user,
-            },
-            statusCode: 200,
-        });
-        return response.status(200).json(successResponse);
-    } catch (error) {
-        const errorResponse = formatErrorResponse(error);
-        return response.status(error.statusCode || 500).json(errorResponse);
-    }
-};
+    const successResponse = formatSuccessResponse({
+        message: "User profile fetched successfully",
+        data: {
+            user,
+        },
+        statusCode: 200,
+    });
+    return response.status(200).json(successResponse);
+});
 
-const updateUserProfileController = async function (request, response) {
-    try {
-        const userId = request.userId;
-        const user = await updateUserProfileById(userId, request.body);
+const updateUserProfileController = asyncHandler(async function (
+    request,
+    response
+) {
+    const userId = request.userId;
+    const user = await updateUserProfileById(userId, request.body);
 
-        const successResponse = formatSuccessResponse({
-            message: "User profile updated successfully",
-            data: {
-                user,
-            },
-            statusCode: 200,
-        });
-        response.status(200).json(successResponse);
-    } catch (error) {
-        const errorResponse = formatErrorResponse(error);
-        return response.status(error.statusCode || 500).json(errorResponse);
-    }
-};
+    const successResponse = formatSuccessResponse({
+        message: "User profile updated successfully",
+        data: {
+            user,
+        },
+        statusCode: 200,
+    });
+    response.status(200).json(successResponse);
+});
 
 module.exports = {
     getLoggedInUserController,
